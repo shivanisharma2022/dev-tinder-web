e1bec6845f7a0e47f26c08ef012ec0e3b74ede8c
shivani sharma <sharmashivani0619@gmail.com>
2025-02-01 17:59:12 +0530

    added navbar footer on all components, fixed the imageUrl issue in all places

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,99 +4,132 @@ import { socketCreateConnection } from '../utils/socket';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { BASE_URL } from '../utils/constant';
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Chat = () => {
-
     const { targetUserId } = useParams();
     const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    const [targetUser, setTargetUser] = useState(null); 
+
     const user = useSelector(store => store.user);
-    const userId = user?.data._id; // getting loggedIn userId from redux store
+    const userId = user?._id;
 
+    // Fetch chat messages and target user info
     const fetchChatMessages = async () => {
-        const chat = await axios.get(BASE_URL + '/chat/' + targetUserId, {
-            withCredentials: true
-        });
+        try {
+            const response = await axios.get(`${BASE_URL}/chat/${targetUserId}`, {
+                withCredentials: true
+            });
 
-        console.log(chat.data.messages);
+            console.log(response.data.messages);
+            
+            // Extract target user details from chat participants
+            const target = response.data.participants.find(participant => participant._id !== userId);
+            setTargetUser(target);
 
-        const chatMessages = chat?.data?.messages.map((msg) => {
-            const { senderId, text } = msg;
-            return {
-                firstName: senderId?.firstName,
-                lastName: senderId?.lastName,
-                text
-            }
-        });
-        setMessages(chatMessages);
-    }
+            // Process messages
+            const chatMessages = response?.data?.messages.map((msg) => {
+                const { senderId, text } = msg;
+                return {
+                    firstName: senderId?.firstName,
+                    lastName: senderId?.lastName,
+                    imageUrl: senderId?.imageUrl, 
+                    text
+                };
+            });
+
+            setMessages(chatMessages);
+        } catch (err) {
+            console.error("Error fetching chat messages:", err);
+        }
+    };
 
     useEffect(() => {
         fetchChatMessages();
     }, []);
 
     useEffect(() => {
-        if (!userId) {
-            return;
-        }
+        if (!userId) return;
+        
         const socket = socketCreateConnection();
-        // as soon as the page loads, socket connection is made and it will emit an event joinChat
-        socket.emit('joinChat', { firstName: user?.data.firstName, userId, targetUserId });
+        socket.emit('joinChat', { firstName: user?.firstName, userId, targetUserId });
 
-        socket.on('receiveMessage', ({ firstName, lastName, text }) => {
+        socket.on('receiveMessage', ({ firstName, lastName, imageUrl, text }) => {
             console.log(firstName + ' : ' + text);
-            setMessages((messages) => [...messages, { firstName, lastName, text }]);
-
+            setMessages((messages) => [...messages, { firstName, lastName, imageUrl, text }]);
         });
 
-        // as soon as the page loads, socket connection is disconnected
         return () => {
             socket.disconnect();
-        }
+        };
     }, [userId, targetUserId]);
 
     const sendMessage = () => {
+        if (!newMessage.trim()) return;
+
         const socket = socketCreateConnection();
         socket.emit('sendMessage', {
-            firstName: user?.data.firstName,
-            lastName: user?.data.lastName,
+            firstName: user?.firstName,
+            lastName: user?.lastName,
             userId,
             targetUserId,
-            text: newMessage
+            text: newMessage,
+            imageUrl: user?.imageUrl
         });
-        setNewMessages("");
-    }
+        setNewMessage("");
+    };
 
     return (
-        <div className='w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
-            <h1 className='p-5 border-b border-gray-600'>Chat</h1>
-            <div className='flex-1 overflow-scroll p-5'>
-                {messages.map((msg, index) => {
-                    return (
+        <>
+            <NavBar>
+                <div className="flex items-center gap-3">
+                    <span className="text-lg font-bold">Welcome, {user?.firstName}!</span>
+                    <img className="w-10 h-10 rounded-full" src={user?.imageUrl} alt="Profile" />
+                </div>
+            </NavBar>
+
+            <div className='w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
+                <h1 className='p-5 border-b border-gray-600 flex items-center gap-3'>
+                    {targetUser && (
+                        <>
+                            <img className="w-10 h-10 rounded-full" src={targetUser.imageUrl} alt="Chat User" />
+                            <span> {targetUser.firstName} {targetUser.lastName}</span>
+                        </>
+                    )}
+                </h1>
+
+                <div className='flex-1 overflow-scroll p-5'>
+                    {messages.map((msg, index) => (
                         <div key={index}
-                        className={'chat ' +
-                            (user?.data.firstName === msg.firstName ? "chat-end" : "chat-start")
-                        }
+                            className={'chat ' + (user?.firstName === msg.firstName ? "chat-end" : "chat-start")}
                         >
-                            <div className="chat-header">
-                                {`${msg.firstName} ${msg.lastName}`}
+                            <div className="chat-header flex items-center gap-2">
+                                <img className="w-8 h-8 rounded-full" src={msg.imageUrl} alt="User" />
+                                <span>{`${msg.firstName} ${msg.lastName}`}</span>
                                 <time className="text-xs opacity-50">2 hours ago</time>
                             </div>
                             <div className="chat-bubble">{msg.text}</div>
                             <div className="chat-footer opacity-50">Seen</div>
                         </div>
-                    );
-                })}
+                    ))}
+                </div>
+
+                <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
+                    <input
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        className='flex-1 border border-gray-500 text-black rounded p-2'
+                        placeholder="Type a message..."
+                    />
+                    <button onClick={sendMessage} className='btn btn-secondary'>Send</button>
+                </div>
             </div>
-            <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-                <input
-                    value={newMessage}
-                    onChange={(e) => setNewMessages(e.target.value)}
-                    className='flex-1 border border-gray-500 text-black rounded p-2'></input>
-                <button onClick={sendMessage} className='btn btn-secondary'>Send</button>
-            </div>
-        </div >
-    )
-}
+
+            <Footer />
+        </>
+    );
+};
 
 export default Chat;
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,16 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 import { BASE_URL } from "../utils/constant";
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { addConnections } from '../utils/connectionsSlice';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionsSlice";
+import NavBar from "./NavBar";
+import Footer from "./Footer"; 
 
 const Connections = () => {
   const connections = useSelector((store) => store.connections);
   const dispatch = useDispatch();
+
   const fetchConnections = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/user/connections", { withCredentials: true });
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
       console.log(res.data.data);
       dispatch(addConnections(res.data.data));
     } catch (err) {
@@ -22,40 +27,40 @@ const Connections = () => {
     fetchConnections();
   }, []);
 
-  if (!connections) return
-  if (connections.length === 0) return <h1> No Connections Found!</h1>
+  if (!connections) return;
+  if (connections.length === 0) return <h1> No Connections Found!</h1>;
 
   return (
-    <div className="text-center my-10">
-      <h1 className="text-bold text-3xl">Connections</h1>
+    <>
+      <NavBar />
+      <div className="text-center my-10">
+        <h1 className="text-bold text-3xl">Connections</h1>
 
-      {connections.map((connection) => {
-        const { _id, firstName, lastName, age, gender, description, imageUrl } =
-          connection;
+        {connections.map((connection) => {
+          const { _id, firstName, lastName, age, gender, description, imageUrl } = connection;
 
-        return (
-          <div key={_id} className="flex justify-between m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
-            <div>
-              <img
-                alt="photo"
-                className="w-20 h-20 rounded-full"
-                src={imageUrl}
-              />
+          return (
+            <div
+              key={_id}
+              className="flex justify-between m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+            >
+              <div>
+                <img alt="photo" className="w-20 h-20 rounded-full" src={imageUrl} />
+              </div>
+              <div className="text-left mx-4">
+                <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+                {age && gender && <p>{age + " " + gender}</p>}
+                <p>{description}</p>
+              </div>
+              <Link to={"/chat/" + _id}>
+                <button className="btn btn-primary">Chat</button>
+              </Link>
             </div>
-            <div className="text-left mx-4">
-              <h2 className="font-bold text-xl">
-                {firstName + " " + lastName}
-              </h2>
-              {age && gender && <p>{age + " " + gender}</p>}
-              <p>{description}</p>
-            </div>
-            <Link to={'/chat/' + _id}>
-              <button className='btn btn-primary'>Chat</button>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <Footer />
+    </>
   );
 };
 
